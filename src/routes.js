2549a3b6946c97518e76df685ec964a49110194a
EMANUEL AUGUSTO DE PAULA SANTOS <FR041725@catolica.ubec>
2020-01-30 21:26:22 -0300

    Ajustes server2

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
 module.exports = function(app) {
     var curriculoController = require('./app/controllers/curriculoController')
 
+    app.route('/', function(req, res){
+        res.json({
+            status: "Api rodando",
+            code: 200
+        });
+    })   
     app.route('/api/curriculo')
     .get(curriculoController.find)
 
